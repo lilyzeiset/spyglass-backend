ef257ad6cdec4e79f192ce0b33a6849536565016
Lily Zeiset <lilyzeiset@gmail.com>
2023-06-28 12:44:20 -0400

    origins

--- src/main/java/com/skillstorm/project/controllers/UserController.java
+++ src/main/java/com/skillstorm/project/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@CrossOrigin(allowCredentials = "true", originPatterns = {"http://d1ulkr0tra4x6n.cloudfront.net","http://localhost:5173","http://lily-spyglass.s3-website-us-east-1.amazonaws.com*","http://lily-spyglass-env.eba-he3agp52.us-east-1.elasticbeanstalk.com*"})
+@CrossOrigin(allowCredentials = "true", originPatterns = {"http://d1ulkr0tra4x6n.cloudfront.net","http://localhost:5173","http://lily-spyglass.s3-website-us-east-1.amazonaws.com","http://lily-spyglass-env.eba-he3agp52.us-east-1.elasticbeanstalk.com"})
 public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
