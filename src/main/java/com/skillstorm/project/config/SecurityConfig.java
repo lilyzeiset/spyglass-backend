36be6592662808bf5c8fcdcb37460f0cfef7c351
Lily Zeiset <lilyzeiset@gmail.com>
2023-06-28 12:43:14 -0400

    change frontend back to s3

--- src/main/java/com/skillstorm/project/config/SecurityConfig.java
+++ src/main/java/com/skillstorm/project/config/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
 		http.cors().configurationSource(request -> {
 			CorsConfiguration corsConfig = new CorsConfiguration();
 			corsConfig.addAllowedOrigin(frontendUrl);
+			corsConfig.addAllowedOrigin("http://lily-spyglass-env.eba-he3agp52.us-east-1.elasticbeanstalk.com");
+			corsConfig.addAllowedOrigin("https://accounts.google.com/o/oauth2/v2/auth");// ?response_type=code&client_id=137424696014-062cnpkc3rkfsihr5fiauaf637nf5l3r.apps.googleusercontent.com&scope=openid%20profile%20email&state=3JL4hOtT4VS5jswtnrcfYRKiOh7KjGBwT7gMA3PsBSA%3D&redirect_uri=http://lily-spyglass-env.eba-he3agp52.us-east-1.elasticbeanstalk.com/login/oauth2/code/google);
 			corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 			corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 			corsConfig.setAllowCredentials(true);
